0554c59fcbe686de492dfd354562c0758ea7e411
TANMAY DEORUKHAKAR <tanmayflicks@gmail.com>
2025-08-11 22:46:22 +0530

    august 11 sums - agressive cows

---
+++ problems/aggresiveCows.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isPossible(vector<int> &arr, int N, int C, int minallowedDist)
+{
+    int cows = 1, lastStallPos = arr[0];
+
+    for (int i = 1; i < N; i++)
+    {
+        if (arr[i] - lastStallPos >= minallowedDist)
+        {
+            cows++;
+            lastStallPos = arr[i];
+        }
+
+        if (cows == C)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int getDistance(vector<int> &arr, int N, int C)
+{
+    sort(arr.begin(), arr.end()); // NlogN
+
+    // start = 1, end = maxVal-minVal;
+
+    int start = 1, end = arr[N - 1] = arr[0], ans = -1;
+
+    while (start <= end) // O(log(Range) * N)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (isPossible(arr, N, C, mid)) // right
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else // left
+        {
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int N = 5, C = 3;
+    vector<int>
+        arr = {1, 2, 8, 4, 9};
+
+    cout << getDistance(arr, N, C) << endl;
+    return 0;
+}

616a589f081bee284c6aee921cf8a13d1f02f385
syskey8 <tanmayflicks@gmail.com>
2025-12-02 16:02:16 +0000

    Added serilization and deserialization with unit tests

--- cpp_projects/vault.cpp
+++ cpp_projects/vault.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <limits>
@@ -9,6 +10,36 @@ struct Entry {
     string username; 
     string password;
   };
+
+string serialize(const vector<Entry>& vault) {
+  ostringstream oss;
+  for(const auto& e : vault) {
+    oss << e.site << "\n";
+    oss << e.username << "\n";
+    oss << e.password << "\n";
+    oss << "---" << "\n";
+  }
+  return oss.str();
+}
+
+vector<Entry> deserialize(const string& text) {
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  vector<Entry> vault;
+  istringstream iss(text);
+  string line;
+
+  while(true) {
+    Entry e;
+    if (!getline(iss, e.site)) break;
+    if (!getline(iss, e.username)) break;
+    if (!getline(iss, e.password)) break;
+    if (!getline(iss, line)) break; //separtor line
+                                    //
+    vault.push_back(e);
+  }
+
+  return vault;
+}
 
 void search_entries(const vector<Entry>& vault) {
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -22,6 +53,7 @@ void search_entries(const vector<Entry>& vault) {
       cout << "Site : " << e.site << endl;
       cout << "Username: " << e.username << endl;
       cout << "Password: " << e.password << endl;
+      cout << "---------------\n";
       found = true;
     }
   }
@@ -63,16 +95,21 @@ void list_entries(vector<Entry>& vault){
 
 int main () {
     vector<Entry> vault;
+    vector<Entry> test;
 
   while(true) {
     int choice;
+    string s1;
     cout << "Menu: " << endl;
     cout << "Select an option: " << endl;
     cout << "1. Add" << endl;
     cout << "2. List" << endl;
     cout << "3. Search" << endl;
     cout << "4. Exit" << endl;
+    cout << "5. Serialize" << endl;
+    cout << "6. deserialize" << endl;
     cin >> choice;
+    cout << "Choice :" << choice << endl;
 
     switch(choice) {
       case 1: 
@@ -86,6 +123,20 @@ int main () {
         break;
       case 4:
         return 0;
+      case 5:
+        s1 = serialize(vault);
+        cout << s1 << endl;
+        break;
+      case 6:
+        test = deserialize(s1);
+        for (size_t i = 0; i < test.size(); ++i) {
+        const auto& e = test[i];
+        std::cout << "[" << i << "]\n";
+        std::cout << "  Site: " << e.site << "\n";
+        std::cout << "  Username: " << e.username << "\n";
+        std::cout << "  Password: " << e.password << "\n";
+    }
+        break;
     }
   }
   return 0;
